9ed163f70907607601803d16ba4bb00396903e93
James McDonald <james@toggen.com.au>
2024-03-21 10:36:46 +1100

    Add curl

---
+++ curlSend.sh
@@ -0,0 +1,8 @@
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -H 'X-Requested-With: XMLHttpRequest' \
+    -H 'Authorization: Token bbbb' \
+    -H 'Accept: application/json' \
+    -H 'X-My-Custom-Header: hijames' \
+    -d @content.json \
+    http://localhost:8765/posts/ajax
